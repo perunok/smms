b219f98a2aebee7b4158a914a570da5d334c7b4a
perun <henokmto444@gmail.com>
2024-04-10 09:16:46 +0300

    feature and bug fix
    detection and response for unknown ISPs added
    status saving procedure modefied for readablility

--- src/main/java/com/act/smms/service/SMSHandler.java
+++ src/main/java/com/act/smms/service/SMSHandler.java
@@ -16,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.act.smms.dto.SMSRequest;
 import com.act.smms.dto.StatusRequest;
+import com.act.smms.enums.ISP;
 import com.act.smms.enums.ISPConfigOption;
 import com.act.smms.enums.SMSStatus;
 import com.act.smms.model.ISPConfig;
@@ -41,6 +42,7 @@ public class SMSHandler {
 
     @SuppressWarnings("null")
     public boolean sendMessage(SMSRequest request) {
+
         try {
             List<ISPConfig> configs = ispConfigRepo.findByOption(ISPConfigOption.DEFAULT);
             if (configs.isEmpty()) {
@@ -48,15 +50,12 @@ public class SMSHandler {
                 return false;
             }
             ISPConfig config = configs.getFirst();
-
-            // Read the response (if needed)
             String username = config.getUsername();
             String password = config.getPassword();
             List<String> recipients = request.getRecipient();
             String message = request.getMessage();
             String smsGatewayUrl = "http://localhost:8000/api/cgi-bin/sendsms";
 
-            // create headers
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
@@ -72,29 +71,47 @@ public class SMSHandler {
             ResponseEntity<StatusRequest> response = null;
 
             for (String recipient : recipients) {
+                Status status = new Status();
                 if (phoneVerifier.isValid(recipient)) {
+                    // if the provided recipient number is not one of the two providers'
+                    if (!(phoneVerifier.getISP(recipient) == ISP.ETHIOTEL
+                            || phoneVerifier.getISP(recipient) == ISP.SAFARICOM)) {
+                        status.setStatus(SMSStatus.FAILD);
+                        status.setMessage(message);
+                        status.setSender(username);
+                        status.setRecipient(recipient);
+                        status.setDate(LocalDateTime.now());
+                        status.setRemark("the number does not refer to a known ISP");
+                        statusRepo.save(status);
+                        continue;
+                    }
 
                     map.put("to", recipient);
                     response = this.restTemplate.postForEntity(smsGatewayUrl, new HttpEntity<>(map, headers),
                             StatusRequest.class);
                     StatusRequest statusRequest = response.getBody();
-
-                    statusRepo.save(new Status(statusRequest.getStatus(), statusRequest.getMessage(),
-                            statusRequest.getSender(),
-                            statusRequest.getRecipient(), LocalDateTime.now(), "this is a remark"));
+                    status.setStatus(statusRequest.getStatus());
+                    status.setMessage(statusRequest.getMessage());
+                    status.setSender(statusRequest.getSender());
+                    status.setRecipient(statusRequest.getRecipient());
+                    status.setDate(LocalDateTime.now());
+                    status.setRemark("remark from the server");
+                    statusRepo.save(status);
                 } else {
-                    statusRepo.save(new Status(SMSStatus.FAILD, message, username, recipient, LocalDateTime.now(),
-                            "the provided phone number is invalid"));
+                    status.setStatus(SMSStatus.FAILD);
+                    status.setMessage(message);
+                    status.setSender(username);
+                    status.setRecipient(recipient);
+                    status.setDate(LocalDateTime.now());
+                    status.setRemark("the provided phone number is invalid");
+                    statusRepo.save(status);
                 }
-
             }
             return true;
-
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
     public boolean configISP(ISPConfig config) {
